274104881f8ff699c8aedc4b6d1ccf9bbde8c21a
Sebastián Calarco <sebastian@calarco.com.ar>
2021-09-30 01:45:34 -0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,148 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+import GlobalStyle from "./globalStyle";
+
+const Main = styled.main`
+    width: 100vw;
+    height: 100vh;
+    overflow-y: overlay;
+    display: grid;
+`;
+
+const List = styled.section`
+    max-width: 30rem;
+    height: 100%;
+    overflow-y: overlay;
+    border: 1px solid red;
+    display: flex;
+    flex-direction: column;
+`;
+
+const Loading = styled.div`
+    @keyframes loading {
+        0% {
+            opacity: 0.5;
+        }
+        50% {
+            opacity: 1;
+        }
+        100% {
+            opacity: 0.5;
+        }
+    }
+
+    padding: 2rem 0;
+    text-align: center;
+    animation-name: loading;
+    animation-duration: 2s;
+    animation-iteration-count: infinite;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const loader = useRef<HTMLDivElement | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [people, setPeople] = useState([
+        {
+            name: "",
+            height: "",
+            mass: "",
+            hair_color: "",
+            skin_color: "",
+            eye_color: "",
+            birth_year: "",
+            gender: "",
+            homeworld: "",
+            films: [""],
+            species: [""],
+            vehicles: [""],
+            starships: [""],
+            created: "",
+            edited: "",
+            url: "",
+        },
+    ]);
+    const [next, setNext] = useState("https://swapi.dev/api/people/");
+
+    const loadPeople = useCallback(() => {
+        next !== "" &&
+            axios.get(next).then((response) => {
+                response.data.results &&
+                    setPeople((people) => [
+                        ...people,
+                        ...response.data.results.filter(
+                            (item: { name: string }, index: number) => {
+                                return (
+                                    response.data.results.findIndex(
+                                        (test: { name: string }) =>
+                                            test.name === item.name
+                                    ) === index
+                                );
+                            }
+                        ),
+                    ]);
+                response.data.next ? setNext(response.data.next) : setNext("");
+                setLoading(false);
+            });
+    }, [next, setPeople]);
+
+    const handleObserver = useCallback((entries) => {
+        if (entries[0].isIntersecting) {
+            setLoading(true);
+        }
+    }, []);
+
+    useEffect(() => {
+        const option = {
+            root: null,
+            rootMargin: "20px",
+            threshold: 0,
+        };
+        loader.current &&
+            new IntersectionObserver(handleObserver, option).observe(
+                loader.current
+            );
+    }, [loader.current, handleObserver]);
+
+    useEffect(() => {
+        loading && loadPeople();
+    }, [loading, loadPeople]);
+
+    return (
+        <>
+            <GlobalStyle />
+            <Main>
+                <List>
+                    {people.map(
+                        (item: {
+                            name: string;
+                            height: string;
+                            mass: string;
+                            hair_color: string;
+                            skin_color: string;
+                            eye_color: string;
+                            birth_year: string;
+                            gender: string;
+                            homeworld: string;
+                            films: string[];
+                            species: string[];
+                            vehicles: string[];
+                            starships: string[];
+                            created: string;
+                            edited: string;
+                            url: string;
+                        }) => (
+                            <h3 key={item.name}>{item.name}</h3>
+                        )
+                    )}
+                    {next !== "" && !loading && (
+                        <Loading ref={loader}>Loading...</Loading>
+                    )}
+                </List>
+            </Main>
+        </>
+    );
 }
 
 export default App;
---
+++ src/globalStyle.ts
@@ -0,0 +1,162 @@
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
+
+export const Device = {
+    laptop: `(min-width: 1024px)`,
+    desktop: `(min-width: 1440px)`,
+};
+
+const GlobalStyle = createGlobalStyle`
+    ${normalize}
+
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+    }
+
+    #root, html, body {
+        height: 100vh;
+    }
+
+    html {
+        font-size: 16px;
+	    scroll-behavior: smooth;
+    }
+    
+    body {
+        --font-family: "Sans";
+
+        --label: 300 0.75rem/1.25rem var(--font-family);
+        --body1: 300 0.9rem/1.25rem var(--font-family);
+        --body2: 500 0.9rem/1.5rem var(--font-family);
+        --subhead1: 300 1rem/1.5rem var(--font-family);
+        --subhead2: 500 1rem/1.75rem var(--font-family);
+        --title: 500 1.25rem/1.75rem var(--font-family);
+        --headline: 300 1.5rem/2rem var(--font-family);
+        --display-1: 300 2rem/2.5rem var(--font-family);
+
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-rendering: optimizeLegibility;
+        font-feature-settings: "kern" 1;
+        font-kerning: normal;
+        background: #000;
+        color: rgba(255, 255, 255, 0.8);
+    }
+
+    p,
+    h6,
+    h5,
+    h4,
+    h3,
+    h2,
+    h1,
+    label {
+        display: inline-block;
+        margin: 0;
+    }
+
+    p {
+        font: var(--body1);
+
+        &::before {
+            content: '';
+            display: inline-block;
+            height: 0;
+            width: 0;
+            margin-top: calc((1 - 1.25) * 0.5em);
+        }
+    }
+
+    h6 {
+        font: var(--body2);
+
+        &::before {
+            content: '';
+            display: inline-block;
+            height: 0;
+            width: 0;
+            margin-top: calc((1 - 1.5) * 0.5em);
+        }
+    }
+
+    h5 {
+        font: var(--subhead1);
+
+        &::before {
+            content: '';
+            display: inline-block;
+            height: 0;
+            width: 0;
+            margin-top: calc((1 - 1.5) * 0.5em);
+        }
+    }
+
+    h4 {
+        font: var(--subhead2);
+
+        &::before {
+            content: '';
+            display: inline-block;
+            height: 0;
+            width: 0;
+            margin-top: calc((1 - 2.25) * 0.5em);
+        }
+    }
+
+    h3 {
+        font: var(--title);
+
+        &::before {
+            content: '';
+            display: inline-block;
+            height: 0;
+            width: 0;
+            margin-top: calc((1 - 2.25) * 0.5em);
+        }
+    }
+
+    h2 {
+        font: var(--headline);
+
+        &::before {
+            content: '';
+            display: inline-block;
+            height: 0;
+            width: 0;
+            margin-top: calc((1 - 2) * 0.5em);
+        }
+    }
+
+    h1 {
+        font: var(--display-1);
+        text-align: center;
+
+        &::before {
+            content: '';
+            display: inline-block;
+            height: 0;
+            width: 0;
+            margin-top: calc((1 - 2.5) * 0.5em);
+        }
+    }
+
+    label {
+        font: var(--label);
+    }
+
+    b {
+        font-weight: 500;
+    }
+
+    button {
+        transition: 0.15s ease-in-out;
+    }
+
+    button:hover {
+        cursor: pointer;
+    }
+`;
+
+export default GlobalStyle;
